403b52bfec0402f8bd15c72b22609a73d066e1bc
Roger Salazar <roger.salazar@unh.edu.pe>
2022-03-23 10:07:05 -0500

    consolidate product of user

--- src/main/java/com/rs/businessaccount/repository/BankAccountRepository.java
+++ src/main/java/com/rs/businessaccount/repository/BankAccountRepository.java
@@ -5,6 +5,7 @@ import com.rs.businessaccount.repository.custom.CustomBusinessAccountRepository;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @EnableReactiveMongoRepositories
@@ -12,4 +13,5 @@ public interface BankAccountRepository extends ReactiveMongoRepository<BankAccou
     Mono<BankAccount> findByAccountNumber(Integer accountNumber);
     Mono<Boolean> existsByAccountNumber(Integer accountNumber);
     Mono<Boolean> existsByIdBankAccount(String idAccount);
+    Flux<BankAccount> findAllByDniUser(Integer dniNumber);
 }
