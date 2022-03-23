848a53d2474110f7692e2fcf98a662a251312046
renato-castillo <renatocastillopingo@gmail.com>
2022-03-22 20:04:05 -0500

    Added debit card functionality

--- src/main/java/com/rs/businessaccount/controller/DebitCardController.java
+++ src/main/java/com/rs/businessaccount/controller/DebitCardController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/api/debit-card")
+@RequestMapping("/debit-card")
 public class DebitCardController {
 
     @Autowired
