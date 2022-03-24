2429a0145417581a8cf1625669aa607a720d45d3
renato-castillo <renatocastillopingo@gmail.com>
2022-03-24 16:33:44 -0500

    Init logic for consumes from debit and credit card

--- src/main/java/com/rs/businessaccount/controller/DebitCardController.java
+++ src/main/java/com/rs/businessaccount/controller/DebitCardController.java
@@ -28,4 +28,10 @@ public class DebitCardController {
     public ResponseEntity<Flux<DebitCard>> findAll(){
         return new ResponseEntity<>(debitCardService.findAll(), HttpStatus.OK);
     }
+
+    @GetMapping("/card-number/{cardNumber}")
+    public ResponseEntity<Mono<DebitCard>> findByCardNumber(@PathVariable String cardNumber) {
+        return new ResponseEntity<>(debitCardService.findByCardNumber(cardNumber), HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/rs/businessaccount/repository/DebitCardRepository.java
+++ src/main/java/com/rs/businessaccount/repository/DebitCardRepository.java
@@ -3,7 +3,10 @@ package com.rs.businessaccount.repository;
 import com.rs.businessaccount.entity.DebitCard;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+import reactor.core.publisher.Mono;
 
 @EnableReactiveMongoRepositories
 public interface DebitCardRepository extends ReactiveMongoRepository<DebitCard, String> {
+
+    Mono<DebitCard> findByCardNumber(String cardNumber);
 }
